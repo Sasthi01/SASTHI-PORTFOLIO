109ef30ae2cf04ae57e85d94cb27052d4ef16373
Sasthi Naidoo <143929550+Sasthi01@users.noreply.github.com>
2025-07-23 16:21:29 +0200

    Create WstGrp21DataSet.cs

---
+++ WstGrp21DataSet/WstGrp21DataSet.cs
@@ -0,0 +1,30 @@
+namespace LGPACKAGING_POS_SYSTEM
+{
+
+
+    partial class WstGrp21DataSet
+    {
+        partial class SaleDataTable
+        {
+        }
+
+        partial class DataTable1DataTable
+        {
+        }
+    }
+}
+
+namespace LGPACKAGING_POS_SYSTEM.WstGrp21DataSetTableAdapters
+{
+    partial class SaleTableAdapter
+    {
+    }
+
+    partial class ItemTableAdapter
+    {
+    }
+
+    public partial class StaffTableAdapter
+    {
+    }
+}
